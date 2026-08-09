731c8f1ce35c84b5c0145226a5edae8dc5d26b97
vsrusakov <virt.p.q@yandex.ru>
2026-08-09 03:09:24 +0300

    Для компонента страницы 404 добавил центрирование текста в стили

--- src/pages/not-fount-404/not-fount-404.tsx
+++ src/pages/not-fount-404/not-fount-404.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
+import styles from './not-found-404.module.css';
 
 export const NotFound404: FC = () => (
-  <h3 className={`pb-6 text text_type_main-large`}>
+  <h3 className={`${styles.textCenter} pb-6 text text_type_main-large`}>
     Страница не найдена. Ошибка 404.
   </h3>
 );
