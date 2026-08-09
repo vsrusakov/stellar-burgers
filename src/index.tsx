32e49bdd3fb99aa1fe4f9dabdd503e5ba99dfcaf
vsrusakov <virt.p.q@yandex.ru>
2026-08-10 00:16:12 +0300

    Подключил хранилище к приложению

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import App from './components/app/app';
+import { Provider } from 'react-redux';
 import router from './router/router';
+import store from './services/store';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
