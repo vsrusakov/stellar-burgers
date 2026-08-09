a5b49e9cb00bf273063c2e5973e2bab965ca543f
vsrusakov <virt.p.q@yandex.ru>
2026-08-10 00:15:42 +0300

    Добавил редьюсер слайса burgerIngredientsSlice в стор

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import burgerIngredientsReducer from './burgerIngredientsSlice';
 
 import {
   TypedUseSelectorHook,
@@ -6,7 +7,9 @@ import {
   useSelector as selectorHook
 } from 'react-redux';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+const rootReducer = combineReducers({
+  burgerIngredients: burgerIngredientsReducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
