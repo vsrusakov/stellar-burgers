f7d45d52cb65739792091ce89d34098407cac7c0
vsrusakov <virt.p.q@yandex.ru>
2026-08-10 00:16:48 +0300

    Добавил получение данных из стора и перенес лоадер

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -1,20 +1,33 @@
-import { useSelector } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  selectIngredients,
+  selectLoadingStatus,
+  selectIngredientsError
+} from '../../services/burgerIngredientsSelectors';
 
 import styles from './constructor-page.module.css';
 
-// import { BurgerIngredients } from '../../components'; TODO: убрать, когда будет готова страница конструктора
-// import { BurgerConstructor } from '../../components';
-// import { Preloader } from '../../components/ui';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { ConstructorPageUI } from '@ui-pages';
+import { fetchIngredients } from '../../services/burgerIngredientsSlice';
+import { Preloader } from '@ui';
 
 export const ConstructorPage: FC = () => {
-  /** TODO: взять переменные из стора */
-  const isIngredientsLoading = false;
-  const ingredients = [];
-  const error = null;
+  const dispatch = useDispatch();
 
-  if (error) {
+  const loadingStatus = useSelector(selectLoadingStatus);
+  const ingredients = useSelector(selectIngredients);
+  const error = useSelector(selectIngredientsError);
+
+  useEffect(() => {
+    dispatch(fetchIngredients());
+  }, [dispatch]);
+
+  if (loadingStatus === 'loading') {
+    return <Preloader />;
+  }
+
+  if (error && loadingStatus === 'failed') {
     return (
       <div className={`${styles.error} text text_type_main-medium pt-4`}>
         {error}
@@ -22,7 +35,7 @@ export const ConstructorPage: FC = () => {
     );
   }
 
-  if (ingredients.length === 0) {
+  if (ingredients.length === 0 && loadingStatus === 'success') {
     return (
       <div className={`${styles.description} text text_type_main-medium pt-4`}>
         Нет игредиентов
@@ -30,5 +43,5 @@ export const ConstructorPage: FC = () => {
     );
   }
 
-  return <ConstructorPageUI isIngredientsLoading={isIngredientsLoading} />;
+  return <ConstructorPageUI />;
 };
